4bb261aceba19a73aa0c2a64a37684c977c89bd6
gavividra <110568727+gavividra@users.noreply.github.com>
2023-04-05 23:49:16 -0400

    Create 4-5-23.py

---
+++ 4-5-23.py
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int root = 0;
+        if(x==1 || x==2){
+            return 1;
+        }
+        for(double i=0; i<=x; i++){
+            if(i*i == x){
+                root = (int)(i);
+                break;
+            }
+            else if(i*i > x){
+                root = (int)(i-1);
+                break;
+            }
+        }
+        return root;
+    }
+}
