0023379bccc8fd8f6ab608f3c3d6fca00357d3dd
gavividra <110568727+gavividra@users.noreply.github.com>
2023-04-07 01:35:14 -0400

    Update 4-6-23.java

--- 4-6-23.java
+++ 4-6-23.java
@@ -1,5 +1,22 @@
-// leetcode #169
+/**
+leetcode #169
 
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+**/
 class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>();
