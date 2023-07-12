a7029a4bff2289fb9d8604c7c9908f84dfa69a38
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-07-11 23:32:54 -0400

    35_SearchInsertPosition.py

--- 35_SearchInsertPosition.py
+++ 35_SearchInsertPosition.py
@@ -1,5 +1,13 @@
 class Solution(object):
     def searchInsert(self, nums, target):
+        n = 0
+        for i in nums:
+            if i == target:
+                return n
+            if i > target:
+                return n
+            n = n+1
+        return n
         """
         :type nums: List[int]
         :type target: int
