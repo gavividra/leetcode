588914e670bb730e278cdee3de5274354ae8cfd1
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-07-13 00:13:56 -0400

    Adding another solution

--- 263_UglyNumber.py
+++ 263_UglyNumber.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def isUgly(self, n):
         if n<1:
             return False
@@ -8,6 +8,19 @@ class Solution(object):
             n = n/3
         while n%2 ==0:
             n = n/2
+        return n==1
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+class Solution2(object):
+    def isUgly(self, n):
+        if n<1:
+            return False
+        for i in [2,3,5]:
+            while n%i==0:
+                n = n/i
         return n==1
         """
         :type n: int
