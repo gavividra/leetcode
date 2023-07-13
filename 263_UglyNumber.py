7df3423f2f07e4ae0760d1a84e5fbf3b6aa51fc3
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-07-13 00:08:04 -0400

    Update 263_UglyNumber.py

--- 263_UglyNumber.py
+++ 263_UglyNumber.py
@@ -1,5 +1,14 @@
 class Solution(object):
     def isUgly(self, n):
+        if n<1:
+            return False
+        while n%5 == 0:
+            n = n/5
+        while n%3 == 0:
+            n = n/3
+        while n%2 ==0:
+            n = n/2
+        return n==1
         """
         :type n: int
         :rtype: bool
