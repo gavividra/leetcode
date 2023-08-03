8c9f453d41ea82f6f214db49ba76bccd2cdd22f6
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-02 23:44:21 -0400

    Update 268_MissingNumber.py

--- 268_MissingNumber.py
+++ 268_MissingNumber.py
@@ -4,3 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
+        n = len(nums) + 1
+        total = (n * (n-1)) // 2
+        for num in nums:
+            total -= num
+        return total
