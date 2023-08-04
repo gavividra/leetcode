3547182185cb6ca4d62aa9b07f922a2ca55683df
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-03 23:59:09 -0400

    Update 345_ReverseVowelsofaString.py

--- 345_ReverseVowelsofaString.py
+++ 345_ReverseVowelsofaString.py
@@ -1,6 +1,16 @@
-class Solution(object):
+class Solution:
     def reverseVowels(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
+        v="aeiouAEIOU"
+        s=list(s)
+        l=0
+        r=len(s)-1
+        while l<r :
+            if s[l] in v and s[r] in v :
+                s[l],s[r]=s[r],s[l]
+                l +=1 
+                r -=1 
+            elif s[l] not in v :
+                l +=1 
+            else :
+                r -=1
+        return ''.join(s)
