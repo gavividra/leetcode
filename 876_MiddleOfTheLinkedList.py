8fcb36ec5dd3dc1a7639a550de16449397a9d2e9
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-06 00:00:38 -0400

    Update 876_MiddleOfTheLinkedList.py

--- 876_MiddleOfTheLinkedList.py
+++ 876_MiddleOfTheLinkedList.py
@@ -9,3 +9,8 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
