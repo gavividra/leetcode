86531dfdf1279a97257f7b30040acfc42fe46fd4
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-07 10:57:18 -0400

    Update 543_DiameterofBinaryTree.py

--- 543_DiameterofBinaryTree.py
+++ 543_DiameterofBinaryTree.py
@@ -10,3 +10,39 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+
+        '''
+        counter = 0
+        if root.left:
+            counter += 1
+            counter += diameterOfBinaryTree(self, root.left)
+        if root.right:
+            counter += 1
+            counter += diameterOfBinaryTree(self, root.right)
+        return counter
+        '''
+
+        '''
+        answer = 0
+        def depth(node):
+            if not node:
+                return 0
+            left = depth(node.left)
+            right = depth(node.right)
+            answer = max(answer, left + right)
+            return 1 + max(right, left)
+        depth(root)
+        return answer
+        '''
+    'Solution'
+        self.diameter=0
+        self.height(root,self.diameter)
+        return self.diameter
+    
+    def height(self,root,diameter):
+        if root is None:
+            return 0
+        lh=self.height(root.left,self.diameter)
+        rh=self.height(root.right,self.diameter)
+        self.diameter=max(self.diameter,lh+rh)
+        return 1+max(lh,rh)
