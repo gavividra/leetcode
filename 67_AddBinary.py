7491eb11e1b6aef01012fb5dc2bf3590559141e9
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-09 22:32:42 -0400

    Update 67_AddBinary.py

--- 67_AddBinary.py
+++ 67_AddBinary.py
@@ -5,3 +5,23 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
+        a1 = list(a)  # Convert string to a list of characters
+        a2 = list(b)  # Convert string to a list of characters
+        length = max(len(a1), len(a2))
+
+        # Make sure both lists have the same length by adding leading zeros
+        while len(a1) < length:
+            a1.insert(0, '0')
+        while len(a2) < length:
+            a2.insert(0, '0')
+
+        carry = 0
+        result = ''
+        for i in range(length-1, -1, -1):
+            carry += (1 if a1[i] == '1' else 0)
+            carry += (1 if a2[i] == '1' else 0)
+            result = ('1' if carry % 2 == 1 else '0') + result
+            carry = (1 if carry >= 2 else 0)  # Change 1 to 2 to correctly handle carry
+        if carry:
+            result = '1' + result
+        return result
