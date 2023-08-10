143905183edfa351e24b081fd23569f85227c2cd
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-10 13:45:27 -0400

    Update 27_RemoveElement.py

--- 27_RemoveElement.py
+++ 27_RemoveElement.py
@@ -5,3 +5,12 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        count = 0  # Initialize count of elements not equal to val
+        left = 0   # Left pointer
+
+        for right in range(len(nums)):
+            if nums[right] != val:
+                nums[left] = nums[right]  
+                left += 1
+                count += 1
+        return count
