0c70fd33ff28716a3fdb71030bf73032ed2312d8
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-09 23:06:42 -0400

    Update 125_ValidPalindrome.py

--- 125_ValidPalindrome.py
+++ 125_ValidPalindrome.py
@@ -4,3 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        s_arr = []
+        for i in s:
+            if i.islower():
+                s_arr.append(i)
+            elif i.isupper():
+                s_arr.append(i.lower())
+        return s_arr[-1:] == s_arr[0:]
+        
