e1d182d58887d508f7574aa00b27c5178bd5a936
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-12 18:39:34 -0400

    Update 229_MajorityElementII.py

--- 229_MajorityElementII.py
+++ 229_MajorityElementII.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        hash_map = {}
+        for num in nums:
+            if num in hash_map:
+                hash_map[num] += 1
+            else:
+                hash_map[num] = 1
+        
+        list = []
+        x = (len(nums)//3) + 1
+        for num in hash_map:
+            if hash_map[num] >= x:
+                list.append(num)
+        return list
