b4f97f298d829b0ce0160f01fe579dca2b35f6e5
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-12 18:31:14 -0400

    Update 169_MajorityElement.py

--- 169_MajorityElement.py
+++ 169_MajorityElement.py
@@ -4,3 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        '''
+        # Hard Version
+        hash_map = {}
+        for num in nums:
+            if num in hash_map:
+                hash_map[num] += 1
+            else:
+                hash_map[num] = 1
+        
+        x = nums[0]
+        for num in hash_map:
+            if hash_map[num] > hash_map[x]:
+                x = num
+        return x
+        '''
