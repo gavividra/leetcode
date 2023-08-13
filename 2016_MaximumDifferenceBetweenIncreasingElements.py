d2ba4b4f7ef1004f2167893f73f96137bf7b489f
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-13 11:56:34 -0400

    Update 2016_MaximumDifferenceBetweenIncreasingElements.py

--- 2016_MaximumDifferenceBetweenIncreasingElements.py
+++ 2016_MaximumDifferenceBetweenIncreasingElements.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        diff = 0
+        for i in range(0,len(nums)-1,1):
+            for j in range(i,len(nums),1):
+                if nums[j]-nums[i] > diff:
+                    diff = nums[j]-nums[i]
+        if diff ==0:
+            return -1
+        else:
+            return diff
