f07e4ec60d2d528ecd6283bfc7f13eaca70f9d25
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-15 16:12:29 -0400

    Update 2656_MaximumSumWithExactlyKElements.py

--- 2656_MaximumSumWithExactlyKElements.py
+++ 2656_MaximumSumWithExactlyKElements.py
@@ -5,3 +5,11 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        result = 0
+        max_num = 0
+        for i in nums:
+            if i>max_num:
+                max_num = i
+        for i in range(0,k,1):
+            result = result + max_num + i
+        return result
