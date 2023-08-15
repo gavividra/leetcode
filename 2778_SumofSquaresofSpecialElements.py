7486ce6f951a75510ab9773cea6960c5b74aa841
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-14 20:19:13 -0400

    Update 2778_SumofSquaresofSpecialElements.py

--- 2778_SumofSquaresofSpecialElements.py
+++ 2778_SumofSquaresofSpecialElements.py
@@ -4,3 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        result = 0
+        for i in range(1,len(nums)+1,1):
+            if len(nums)%i == 0:
+                result = result + nums[i-1]**2
+
+        return result
