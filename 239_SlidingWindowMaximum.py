d08750468b63439e39c528df908416ddbffd4d08
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-16 11:01:32 -0400

    Update 239_SlidingWindowMaximum.py

--- 239_SlidingWindowMaximum.py
+++ 239_SlidingWindowMaximum.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        '''
         #Time Limit Exceeded because it loops through every window
         result = []
         for i in range(0,len(nums)-k+1,1):
@@ -13,4 +14,22 @@ class Solution(object):
                 if nums[j] > max_num:
                     max_num = nums[j]
             result.append(max_num)
+        return result
+        '''
+
+
+        #This version eliminates the numbers prior to the largest element in the window
+        n=len(nums)
+        seen=[]
+        result=[]   
+        for i in range(n):
+            if seen and seen[0]==i-k:
+                seen.pop(0)
+
+            while seen and nums[seen[-1]]<nums[i]:
+                seen.pop()
+
+            seen.append(i)
+            if i>=k-1:
+                result.append(nums[seen[0]])
         return result
