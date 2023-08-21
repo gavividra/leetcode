0f795f3ac49044abc2f625f4f1f31f2950d532d1
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-20 22:25:37 -0400

    Update 231_PowerofTwo.py

--- 231_PowerofTwo.py
+++ 231_PowerofTwo.py
@@ -4,3 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        #Mathmatical reasoning
+        num = n
+        if num <= 0:
+            return False
+        if num > 1:
+            while num > 1:
+                if num % 2 !=0:
+                    return False
+                num = num/2
+            return True
+        if num < 1:
+            while num < 1:
+                num = num*2
+            if num != 1:
+                return False
+        return True
