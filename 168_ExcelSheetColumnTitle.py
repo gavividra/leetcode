0863749bac61672fc1714508d09e11a26aec4b28
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-23 18:16:39 -0400

    Update 168_ExcelSheetColumnTitle.py

--- 168_ExcelSheetColumnTitle.py
+++ 168_ExcelSheetColumnTitle.py
@@ -4,3 +4,10 @@ class Solution(object):
         :type columnNumber: int
         :rtype: str
         """
+        result=''
+        while columnNumber > 0:
+            columnNumber -= 1
+            remainder = columnNumber % 26
+            result = chr(65 + remainder) + result
+            columnNumber = columnNumber // 26
+        return result
