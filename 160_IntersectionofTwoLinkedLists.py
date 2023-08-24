f6377218451696f837f291ee3e6d44b9d4f8817a
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-24 18:10:49 -0400

    Update 160_IntersectionofTwoLinkedLists.py

--- 160_IntersectionofTwoLinkedLists.py
+++ 160_IntersectionofTwoLinkedLists.py
@@ -10,3 +10,31 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
+
+        # Confused by return value -- Version 1
+        intersectVal = 0
+        change = False
+        listA = []
+        listB = []
+        skipA = 0
+        skipB = 0
+        while headA:
+            listA.append(headA)
+            headA = headA.next
+        while headB:
+            listB.append(headB)
+            if headB.val in listA and change == False:
+                intersectVal = headB.val
+                change = True  
+            headB = headB.next
+        while headA:
+            skipA += 1
+            if headA.val == intersectVal:
+                break
+            headA = headA.next
+        while headB:
+            skipB += 1
+            if headB.val == intersectVal:
+                break
+            headB = headB.next
+        return "Intersected at '%d'" % (intersectVal)
