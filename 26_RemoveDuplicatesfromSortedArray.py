9fa4a52047f1dd0e5d6790ecd51b186621cc2456
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-25 15:06:25 -0400

    Update 26_RemoveDuplicatesfromSortedArray.py

--- 26_RemoveDuplicatesfromSortedArray.py
+++ 26_RemoveDuplicatesfromSortedArray.py
@@ -4,3 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        result = 1
+        array = []
+        for i in range(1,len(nums),1):
+            if nums[i-1] != nums[i]:
+                nums[result] = nums[i]
+                result +=1
+        return result
