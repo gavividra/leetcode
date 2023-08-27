d2b731bcaf87f4586074c5a25138173b57c64ee7
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-27 17:51:57 -0400

    Update 28_FindtheIndexoftheFirstOccurrenceinaString.py

--- 28_FindtheIndexoftheFirstOccurrenceinaString.py
+++ 28_FindtheIndexoftheFirstOccurrenceinaString.py
@@ -5,6 +5,19 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        '''
+        V1
+        check = -1
+        for i in range(0,len(haystack)):
+            if haystack[i] in needle[0]:
+                check = i
+                for j in range(0,len(needle)):
+                    if haystack[i+j] != needle[j]:
+                        check = -1
+                if check != -1:
+                    return check
+        return -1
+        '''
         for i in range(len(haystack) - len(needle) + 1):
             if haystack[i:i + len(needle)] == needle:
                 return i
