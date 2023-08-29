2a3f6f95295d9ae84baf3602dc324b0c332f352c
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-28 23:14:47 -0400

    Update 1323_Maximum69Number.py

--- 1323_Maximum69Number.py
+++ 1323_Maximum69Number.py
@@ -4,3 +4,13 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
+        string = str(num)
+        check = True
+        result = ""
+        for i in string:
+            if check and i=='6':
+                check = False
+                result += '9'
+            else:
+                result += i
+        return int(result)
