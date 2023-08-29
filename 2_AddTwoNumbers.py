27fdba0b130fcc0a4ec34ada433bee39adadfbc1
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-29 11:52:50 -0400

    Update 2_AddTwoNumbers.py

--- 2_AddTwoNumbers.py
+++ 2_AddTwoNumbers.py
@@ -10,3 +10,24 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        head = ListNode(0)
+        tail = head
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry != 0:
+            val1 = l1.val if l1 is not None else 0
+            val2 = l2.val if l2 is not None else 0
+            val = val1 + val2 + carry
+
+            carry = val//10
+            val = val%10
+            
+            node = ListNode(val)
+            tail.next = node
+            tail = tail.next
+
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+
+        return head.next
+
