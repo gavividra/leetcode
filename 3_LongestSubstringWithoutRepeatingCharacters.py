c3a482f454ceaba46cb62b8b01f04dc3655534b4
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-30 16:04:30 -0400

    Update 3_LongestSubstringWithoutRepeatingCharacters.py

--- 3_LongestSubstringWithoutRepeatingCharacters.py
+++ 3_LongestSubstringWithoutRepeatingCharacters.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # Version 1
+        counter = 0
+        result = 0
+        bank = []
+        for i in s:
+            if i in bank:
+                result = max(counter,result)
+                counter = 0
+                bank = []
+            else:
+                counter += 1
+                bank.append(i)
+        return max(result,counter)
