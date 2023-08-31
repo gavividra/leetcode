6f79bd8c7e1c2e6d8887bb5687025831af8604e0
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-31 15:07:53 -0400

    Update 3_LongestSubstringWithoutRepeatingCharacters.py

--- 3_LongestSubstringWithoutRepeatingCharacters.py
+++ 3_LongestSubstringWithoutRepeatingCharacters.py
@@ -37,3 +37,19 @@ class Solution(object):
                 counter += 1
                 bank.append(i)
         return max(result,counter)
+
+        # Version 3 - Taken from solutions
+        seen = {}
+        left = 0
+        length = 0
+
+        for right, char in enumerate(s):
+
+            if char in seen and seen[char] >= left:
+                left = seen[char] + 1
+            else:
+                length = max(length, right - left + 1)
+            
+            seen[char] = right
+        
+        return length
