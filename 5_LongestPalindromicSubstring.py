f4991f261f71087f37eeada3d33b98ecb6f6e84a
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-08-31 16:49:05 -0400

    Update 5_LongestPalindromicSubstring.py

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -3,4 +3,26 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
-        """
+        """       
+        # Trial 1
+        seen = ['A','A']
+        left = 1
+        string = ""
+        result = ""
+        for right, char in enumerate(s):
+            if char != seen[right] and char != seen[right-left+2]:
+                string = ""
+                left = 1
+            if char == seen[right]:
+                left+=1
+                string = seen[-1]
+            if char == seen[right-left+2]:
+                string = seen[right-left+2] + string + char
+                left+=1
+            if len(string) > len(result):
+                result = string
+            print(string)
+            print(result)
+            print("_____")
+            seen.append(char)
+        return result
