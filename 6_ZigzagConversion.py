c3bc9b17dab49235eab43cc9a381279e06d0fb03
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-01 18:53:15 -0400

    Update 6_ZigzagConversion.py

--- 6_ZigzagConversion.py
+++ 6_ZigzagConversion.py
@@ -5,3 +5,18 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        if numRows == 1 or numRows >= len(s):
+            return s
+        result = [""] * numRows
+        index = 0
+        step = 1 
+
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return "".join(result)
