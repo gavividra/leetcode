52329bb6e7adf13424df989cb3c1543138e095de
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-01 19:01:33 -0400

    Update 6_ZigzagConversion.py

--- 6_ZigzagConversion.py
+++ 6_ZigzagConversion.py
@@ -20,3 +20,25 @@ class Solution(object):
             index += step
 
         return "".join(result)
+
+        '''
+        # Version 2
+        if numRows == 1 or numRows >= len(s):
+            return s
+        result = []
+        index = 0
+        step = 1 
+
+        for char in s:
+            if len(result) <= index:
+                result.append(char)
+            else:
+                result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        return "".join(result)
+        '''
+        
