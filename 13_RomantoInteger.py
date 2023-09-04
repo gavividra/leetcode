4c692d6623e10d0187cf842e8d0a57389677e35f
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-04 13:01:15 -0400

    Update 13_RomantoInteger.py

--- 13_RomantoInteger.py
+++ 13_RomantoInteger.py
@@ -4,3 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        bank = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        result = 0
+        prev = None
+        for i in s[::-1]:
+            if i == 'I' and (prev == 'V' or prev == 'X'):
+                result -= 1
+            elif i == 'X' and (prev == 'L' or prev == 'C'):
+                result -= 10
+            elif i == 'C' and (prev == 'D' or prev == 'M'):
+                result -= 100            
+            else:
+                val = bank.get(i)
+                result += val
+            prev = i
+        return result
