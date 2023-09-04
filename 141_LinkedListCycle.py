0c67f5e8a55fa444073e12b1a5445ef64f522e7d
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-03 21:16:12 -0400

    Update 141_LinkedListCycle.py

--- 141_LinkedListCycle.py
+++ 141_LinkedListCycle.py
@@ -10,3 +10,21 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        # Version 1
+        if not head:
+            return False
+        curr = head
+        while curr:
+            print("curr: " + str(curr.val))
+            if curr.next and curr.next == curr:
+                return True
+            temp = head
+            while temp and temp != curr:
+                print("temp: " + str(temp.val))
+                if curr.next == temp:
+                    return True
+                temp = temp.next
+            curr = curr.next
+        return False
+
+        
