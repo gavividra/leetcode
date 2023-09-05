c8788be6e82da2130a9cd09e179d4fde67c287f7
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-05 09:14:52 -0400

    Update 138_CopyListwithRandomPointer.py

--- 138_CopyListwithRandomPointer.py
+++ 138_CopyListwithRandomPointer.py
@@ -13,3 +13,26 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
+        if not head:
+            return None
+        
+        node_mapping = {}
+
+        temp = head
+        while temp:
+            new_node = Node(temp.val)
+            node_mapping[temp] = new_node
+            temp = temp.next
+        
+        temp = head
+        while temp:
+            if temp.random:
+                node_mapping[temp].random = node_mapping[temp.random]
+            if temp.next:
+                node_mapping[temp].next = node_mapping[temp.next]
+            temp = temp.next
+        
+        return node_mapping[head]
+        
+
+            
