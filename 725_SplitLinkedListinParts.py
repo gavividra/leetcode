c554bd93c78086d5f4416032f171b51bb3b0e68a
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-07 08:31:35 -0400

    Update 725_SplitLinkedListinParts.py

--- 725_SplitLinkedListinParts.py
+++ 725_SplitLinkedListinParts.py
@@ -10,3 +10,39 @@ class Solution(object):
         :type k: int
         :rtype: List[ListNode]
         """
+        # Step 1: Calculate the length of the linked list
+        length = 0
+        temp = head
+        while temp:
+            length += 1
+            temp = temp.next
+        
+        # Step 2: Calculate the size of each part and the remainder
+        part_size = length // k
+        remainder = length % k
+
+        # Step 3: Split the linked list into k parts
+        result = []
+        temp = head
+
+        for i in range(k):
+            part_head = temp
+            part_length = part_size + (1 if i < remainder else 0)
+
+            if part_head:
+                # Find the tail of the current part
+                for j in range(part_length - 1):
+                    temp = temp.next
+
+                # Save the next node and disconnect the current part
+                Next = temp.next
+                temp.next = None
+                result.append(part_head)
+
+                # Move to the next part
+                temp = Next
+            else:
+                # If there are no more nodes, append None
+                result.append(None)
+
+        return result
