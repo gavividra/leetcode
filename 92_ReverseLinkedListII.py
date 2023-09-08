95e20222eeedc2b18be623368496257b1b21220f
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-08 19:03:12 -0400

    Update 92_ReverseLinkedListII.py

--- 92_ReverseLinkedListII.py
+++ 92_ReverseLinkedListII.py
@@ -11,3 +11,30 @@ class Solution(object):
         :type right: int
         :rtype: ListNode
         """
+
+        if not head:
+            return None
+        
+        # Create a dummy node to simplify reversing the first part of the list
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        # Move prev to the node just before the left-th node
+        for i in range(left - 1):
+            prev = prev.next
+
+        # Initialize current and next pointers
+        current = prev.next
+        next_node = None
+
+        # Reverse the sublist from left to right
+        for i in range(right - left):
+            next_node = current.next
+            current.next = next_node.next
+            next_node.next = prev.next
+            prev.next = next_node
+
+        return dummy.next
+
+        
