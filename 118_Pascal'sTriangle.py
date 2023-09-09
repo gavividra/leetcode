94e2daf308fe8ca43fd8b3aebe2fe9f8bf967bd7
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-08 20:09:28 -0400

    Update 118_Pascal'sTriangle.py

--- 118_Pascal'sTriangle.py
+++ 118_Pascal'sTriangle.py
@@ -4,3 +4,18 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+
+        matrix = [[] for _ in range(numRows)]
+        if numRows >= 1:
+            matrix[0] = [1]
+        if numRows >= 2:
+            matrix[1] = [1,1]
+
+        for i in range(2,numRows):
+            matrix[i].append(1)
+            for j in range(1,i):
+                num = matrix[i-1][j-1] + matrix[i-1][j]
+                matrix[i].append(num)
+            matrix[i].append(1)            
+        return matrix
+
