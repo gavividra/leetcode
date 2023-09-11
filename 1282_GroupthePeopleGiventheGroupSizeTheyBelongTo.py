ac12bcd39726dd3b9a91c7d10b99f0110dde0d83
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-11 16:53:01 -0400

    Update 1282_GroupthePeopleGiventheGroupSizeTheyBelongTo.py

--- 1282_GroupthePeopleGiventheGroupSizeTheyBelongTo.py
+++ 1282_GroupthePeopleGiventheGroupSizeTheyBelongTo.py
@@ -4,3 +4,17 @@ class Solution(object):
         :type groupSizes: List[int]
         :rtype: List[List[int]]
         """
+        groups = {}
+        result = []
+        
+        for i, size in enumerate(groupSizes):
+            if size not in groups:
+                groups[size] = [i]
+            else:
+                groups[size].append(i)
+            
+            if len(groups[size]) == size:
+                result.append(groups[size])
+                groups[size] = []
+        
+        return result
