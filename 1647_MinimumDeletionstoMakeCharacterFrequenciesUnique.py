02ab035efe7df1fe746fbd053d623df731c999fa
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-12 17:17:55 -0400

    Update 1647_MinimumDeletionstoMakeCharacterFrequenciesUnique.py

--- 1647_MinimumDeletionstoMakeCharacterFrequenciesUnique.py
+++ 1647_MinimumDeletionstoMakeCharacterFrequenciesUnique.py
@@ -4,3 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        '''
+        my_dict = {}
+
+        for char in s:
+            if char not in my_dict:
+                my_dict[char] = 1
+            else:
+                my_dict[char] += 1
+        
+        result = 0
+        my_dict = sorted(my_dict.items(), key=lambda item: item[1], reverse=False)
+        quantity = 0
+
+        for char,value in my_dict:
+            if value != quantity+1:
+                result += abs(value-quantity)
+        return result
+        '''
