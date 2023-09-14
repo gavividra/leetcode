65d35439b2c532ac69a76482d6c873a29a14114d
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-14 17:13:54 -0400

    Update 332_ReconstructItinerary.py

--- 332_ReconstructItinerary.py
+++ 332_ReconstructItinerary.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     def findItinerary(self, tickets):
         """
@@ -6,6 +5,7 @@ class Solution(object):
         :rtype: List[str]
         """
         
+        '''
         # Version 1 -- Needs a while loop until tickets is empty
         airport = "JFK"
         result = []
@@ -15,4 +15,28 @@ class Solution(object):
                 result.append(airport)
                 airport = ticket[1]
                 tickets.remove(ticket)
+        return result
+        '''
+
+        # Version 2 -- Needs to find longest path
+        def get_next_airport(airport):
+            for i, ticket in enumerate(tickets):
+                if ticket[0] == airport:
+                    return i, ticket[1]
+            return None, None
+
+        result = []
+        tickets.sort()
+        airport = "JFK"
+
+        while tickets:
+            index, next_airport = get_next_airport(airport)
+            if next_airport:
+                result.append(airport)
+                airport = next_airport
+                del tickets[index]
+            else:
+                break
+
+        result.append(airport)
         return result
