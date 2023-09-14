0013807bbbabc652776e24984d13b49463fd853c
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-14 17:14:20 -0400

    Update 332_ReconstructItinerary.py

--- 332_ReconstructItinerary.py
+++ 332_ReconstructItinerary.py
@@ -18,6 +18,7 @@ class Solution(object):
         return result
         '''
 
+        '''
         # Version 2 -- Needs to find longest path
         def get_next_airport(airport):
             for i, ticket in enumerate(tickets):
@@ -39,4 +40,32 @@ class Solution(object):
                 break
 
         result.append(airport)
+        return result
+        '''
+
+        # Version 3 -- exceding time limit
+        def get_next_airport(airport):
+            for i, ticket in enumerate(tickets):
+                if ticket[0] == airport:
+                    return i, ticket[1]
+            return None, None
+
+        def dfs(airport):
+            if len(result) == len(tickets) + 1:
+                return True  # All tickets used
+            index, next_airport = get_next_airport(airport)
+            while next_airport:
+                result.append(airport)
+                if dfs(next_airport):
+                    return True  # Found a valid path
+                result.pop()  # Backtrack
+                index, next_airport = get_next_airport(airport)
+            return False
+
+        result = []
+        tickets.sort()
+        airport = "JFK"
+
+        dfs(airport)
+
         return result
