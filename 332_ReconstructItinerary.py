f7c4029a7c992338b123e0dd2c8f753e92185759
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-14 17:13:38 -0400

    Update 332_ReconstructItinerary.py

--- 332_ReconstructItinerary.py
+++ 332_ReconstructItinerary.py
@@ -1,6 +1,18 @@
+
 class Solution(object):
     def findItinerary(self, tickets):
         """
         :type tickets: List[List[str]]
         :rtype: List[str]
         """
+        
+        # Version 1 -- Needs a while loop until tickets is empty
+        airport = "JFK"
+        result = []
+        index = 0
+        for ticket in tickets:
+            if ticket[0] == airport:
+                result.append(airport)
+                airport = ticket[1]
+                tickets.remove(ticket)
+        return result
