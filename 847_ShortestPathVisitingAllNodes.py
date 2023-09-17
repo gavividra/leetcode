36e3d2c89f4e41c518786e40eb2baa4b7b599b89
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-16 20:54:57 -0400

    Update 847_ShortestPathVisitingAllNodes.py

--- 847_ShortestPathVisitingAllNodes.py
+++ 847_ShortestPathVisitingAllNodes.py
@@ -4,3 +4,35 @@ class Solution(object):
         :type graph: List[List[int]]
         :rtype: int
         """
+        
+        n = len(graph)
+        endState = (1<<n)-1
+
+        path_len = 0
+        queue = collections.deque()
+        visited = set()
+
+        for i in range(n):
+            queue.append((i, 1<<i))
+        
+        while queue:
+            for i in range(len(queue)):
+                node, curState = queue.popleft()
+
+                if curState == endState:
+                    return path_len
+
+                if (node, curState) in visited:
+                    continue
+                
+                visited.add((node, curState))
+                for next_node in graph[node]:
+                    queue.append((next_node, curState | 1<<next_node))
+            path_len+=1
+        
+        return -1
+
+
+
+
+                
