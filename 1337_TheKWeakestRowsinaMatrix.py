61ac6dfe94d9a71a81a3994fdec79061a0dbac45
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-09-17 23:31:35 -0400

    Update 1337_TheKWeakestRowsinaMatrix.py

--- 1337_TheKWeakestRowsinaMatrix.py
+++ 1337_TheKWeakestRowsinaMatrix.py
@@ -5,11 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        
+        # Version 1
         row_counts = [(i, sum(row)) for i, row in enumerate(mat)]
-        
         row_counts.sort(key=lambda x: (x[1], x[0]))
-        
         result = [x[0] for x in row_counts[:k]]
-        
         return result
+
+        # Version 2
+        temp = []
+        for i, j in enumerate(mat):
+            sumAndIdx = (sum(j), i)
+            temp.append(sumAndIdx)
+
+        temp.sort()
+        return [i[1] for i in temp[:k]]
