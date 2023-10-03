d0bcfb82390f40ca2268176a5514630038b85045
Gavi Vidra <110568727+gavividra@users.noreply.github.com>
2023-10-02 19:16:10 -0700

    Update 1_TwoSum.py

--- 1_TwoSum.py
+++ 1_TwoSum.py
@@ -5,8 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+
+        # # V1
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
         
+        # V2
+        dict = {}
+        for index, num in enumerate(nums):
+          diff = target - num
+          if(diff in dict):
+            return [index, dict.get(diff)]
+          dict[num] = index
